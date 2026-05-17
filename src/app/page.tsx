925826e5d5a3b5113f3f246bc412f5df92126c70
bl-kishor <bl.kishor.90@gmail.com>
2026-05-17 16:20:56 +0530

    Initial  commit - next-js first project

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import homeImg from "public/home.jpg";
+import Hero from "@/components/hero";
+
+export default function Home() {
+  return (
+    <div>
+      <Hero
+        imgData={homeImg}
+        imgAlt="Cars factory"
+        title="Professional Cloud Hosting"
+      />
+    </div>
+  );
+}
